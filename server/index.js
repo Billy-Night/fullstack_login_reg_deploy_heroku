83c0a06c71fa301cfa6622106da6fd30adebf535
Billy <billy.m.nightingale@gmail.com>
2022-06-19 20:18:27 +0200

    initial deployement of app

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+//This piece of code allows the Node and React app to be deployed on the same domain.
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
